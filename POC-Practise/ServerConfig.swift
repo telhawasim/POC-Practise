9920db14882e4dc76e5683ed5d55cc34f9ec628a
telha.wasim <telha.wasim@synavos.com>
2022-12-13 13:36:18 +0500

    Adding files

---
+++ POC-Practise/ServerConfig.swift
@@ -0,0 +1,36 @@
+//
+//  ServerConfig.swift
+//  POC-Practise
+//
+//  Created by Telha Wasim on 12/12/2022.
+//
+
+import Foundation
+
+enum BaseURL: String {
+    case LOCAL = "https://abc.local.com"
+    case DEV = "https://abc.dev.com"
+    case QA = "https://abc.qa.com"
+}
+
+public class ServerConfig {
+    static let shared: ServerConfig = ServerConfig()
+    
+    var baseURL: String?
+    
+    func serverConfig() {
+        
+        #if LOCAL
+        // dummy text
+        // change
+        self.baseURL = BaseURL.LOCAL.rawValue
+        
+        #elseif DEV
+        self.baseURL = BaseURL.DEV.rawValue
+        
+        #elseif QA
+        self.baseURL = BaseURL.QA.rawValue
+        
+        #endif
+    }
+}
---
+++ Pods/Target Support Files/Pods-POC-Practise-POC-PractiseUITests/Pods-POC-Practise-POC-PractiseUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_POC_Practise_POC_PractiseUITests : NSObject
+@end
+@implementation PodsDummy_Pods_POC_Practise_POC_PractiseUITests
+@end
